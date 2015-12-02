eff3caa89fd643ae6a083be1012c240e47f9049c
Shashank Gupta <shashankgupta3@gmail.com>
2015-12-01 21:12:51 -0500

    ...

--- test/test.js
+++ test/test.js
@@ -2,26 +2,42 @@ var expect = require('chai').expect
 var D = require('../src/main').D
 
 // ***detectWin***
-var xWins = D.detectWin.bind(null, 'x')
-var oWins = D.detectWin.bind(null, 'o')
+var dw = D.detectWin
+var xw = dw.bind(null, 'x')
+var ow = dw.bind(null, 'o')
 
-// Dumbs
-expect(xWins('fffffffff')).to.equal(false)
-expect(oWins('fffffffff')).to.equal(false)
+// Empty
+expect(xw('fffffffff')).equal(false)
+expect(ow('fffffffff')).equal(false)
 
 // Straight Line
-expect(xWins('xxxxxxxxx')).to.equal(true)
-expect(oWins('ooooooooo')).to.equal(true)
+expect(xw('xxxxxxxxx')).equal(true)
+expect(ow('ooooooooo')).equal(true)
 
 // Diags
-expect(xWins('xoooxooox')).to.equal(true)
-expect(xWins('ooxoxoxoo')).to.equal(true)
+expect(xw('xoooxooox')).equal(true)
+expect(xw('ooxoxoxoo')).equal(true)
 
-expect(oWins('oxxxoxxxo')).to.equal(true)
-expect(oWins('xxoxoxoxx')).to.equal(true)
+expect(ow('oxxxoxxxo')).equal(true)
+expect(ow('xxoxoxoxx')).equal(true)
 
 // Throws
-expect(xWins).to.throw(Error)
-expect(oWins).to.throw(Error)
+expect(xw).throw(Error)
+expect(ow).throw(Error)
+
+// ***detectTwoInARow***
+var dtiar = D.detectTwoInARow
+var dtiarX = dtiar.bind('x')
+var ditarO = dtiar.bind('o')
+
+
+
+// ***detectFreeSpaces***
+var dfs = D.detectFreeSpaces
+
+// Empty
+expect(dfs('fffffffff')).deep.equal([0,1,2,3,4,5,6,7,8])
+expect(dfs('oofofofoo')).deep.equal([2, 4, 6])
+expect(dfs('fxxxfxxxf')).deep.equal([0, 4, 8])
 
 console.log('All tests passed')
