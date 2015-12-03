e29d35e90c6e84bff1ce0a2d9a245fc5c02a1db7
Shashank Gupta <shashankgupta3@gmail.com>
2015-12-03 14:31:53 -0500

    ...

--- src/lib/AI.js
+++ src/lib/AI.js
@@ -15,22 +15,35 @@ function AI(mark, grid) {
     }
 
     function bestChoice() {
+        // If X and first move, 
+        // move top left corner to be as aggressive as possible.
         var isEmpty = detect.isEmpty()
         if (isEmpty) {
             return 0
         } 
+        // If there is a way to win, take it.
         var waysToWin = detect.waysToWin(mark)
         if (waysToWin.length) {
             return waysToWin[0]
         }
+
+        // If your opponent can win, block them.
         var opponentMark = mark === X ? O : X
         var cellsToBlock = detect.waysToWin(opponentMark)
         if (cellsToBlock.length) {
             console.log('blocking opponent 2-in-a-row')
             return cellsToBlock[0]
         }
+
+        // Detect children that are forks
+        // Create fork if possible.
         var children = detect.children(mark)
-        var childrenDetects = children.map(D)
+        var forks = children.filter(detect.childIsFork.bind(null, mark))
+        if (forks.length)
+            return forks[0].path
+
+        // Block opponent's chance to fork
+        // Preferably as aggressively as possible.
         return randomChoice()
     }
 
--- src/lib/detect.js
+++ src/lib/detect.js
@@ -63,6 +63,10 @@ function D(grid) {
         })
     }
 
+    function childIsFork(mark, child) {
+        return D(child.grid).waysToWin(mark).length > 1
+    }
+
 
 
     return {
